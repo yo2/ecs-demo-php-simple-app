b8d7a07c0477b14b62ffcb53d8c9a6e2f320ddc6
David Williams <david.williams@polarseven.com>
2018-03-03 11:29:56 +1100

    updates

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Pimple PHP App</title>
+        <title>PolarSeven</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #4286f4;}</style>
@@ -16,9 +16,9 @@
         <div class="container">
             <div class="hero-unit">
                 <img src="assets/images/p7.png" width="250" height="120">
-                <h1>Simple PHP App Version 3 With Link</h1>
+                <h1>PolarSeven AWS ECS Pipeline Demo</h1>
                 <h2>Congratulations</h2>
-                <h2>THIS IS VERSION 1.3</h2>
+                <h2>THIS IS VERSION 1.1</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
