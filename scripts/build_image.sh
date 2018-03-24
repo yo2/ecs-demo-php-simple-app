2da0356f57a08046f912b9a85b9031e58d00ef25
David Williams <david.williams@polarseven.com>
2018-03-24 11:42:58 +1100

    repo color  #aef7af

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,7 +2,7 @@
 echo "Creating the image" >> /var/log/kops.log
 REPOSITORY_URI=`cat /opt/k8s/ecr.txt`
 REPO=$(echo "${REPOSITORY_URI}" |awk -F"/" '{ print $2 }')
-
+echo "Repository = $REPO" >> /var/log/kops.log
 echo "$REPOSITORY_URI" >> /var/log/kops.log
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
 TAG=`date "+%d%H%M%S"`
@@ -11,8 +11,7 @@ echo "$IMAGE_URI" >> /var/log/kops.log
 echo $(grep "background-color" /opt/k8s/codedeploy/scripts/src/index.php) >> /var/log/kops.log
 echo "$(which docker)" &>> /var/log/kops.log
 echo "$(docker build -t $REPO .)" &>> /var/log/kops.log
-echo "$(docker tag $REPO $IMAGE_URI)" &>> /var/log/kops.log
+echo "$(docker tag $REPO:latest $IMAGE_URI)" &>> /var/log/kops.log
 echo "$(docker push $IMAGE_URI)"  &>> /var/log/kops.log
-sleep 30
 echo "$(docker image ls $IMAGE_URI)" &>> /var/log/kops.log
 echo "$(kubectl set image deployment/website1 website1=$IMAGE_URI)" &>> /var/log/kops.log
