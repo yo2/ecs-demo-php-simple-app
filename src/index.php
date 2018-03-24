8768ed57d2c7731e6d69e132085de257ed0d65aa
David Williams <david.williams@polarseven.com>
2018-03-24 12:09:54 +1100

    color  #f4bfac

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@
         <title>PolarSeven</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color:  #aef7af;}</style>
+        <style>body {margin-top: 40px; background-color:  #f4bfac;}</style>
         <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
