305ed16595bf0ae15d1dc77b321373753fb1a344
David Williams <david.williams@polarseven.com>
2018-03-24 11:16:00 +1100

    remove quotes

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -10,8 +10,9 @@ IMAGE_URI="${REPOSITORY_URI}:${TAG}"
 echo "$IMAGE_URI" >> /var/log/kops.log
 echo $(grep "background-color" /opt/k8s/codedeploy/scripts/src/index.php) >> /var/log/kops.log
 echo "$(which docker)" &>> /var/log/kops.log
-echo "$(docker build -t "$REPO" .)" &>> /var/log/kops.log
-echo "$(docker tag $REPO "$IMAGE_URI")" &>> /var/log/kops.log
-echo "$(docker push "$IMAGE_URI")"  &>> /var/log/kops.log
-sleep 20
-echo "$(kubectl set image deployment/website1 website1=\"$IMAGE_URI\")" &>> /var/log/kops.log
+echo "$(docker build -t $REPO .)" &>> /var/log/kops.log
+echo "$(docker tag $REPO $IMAGE_URI)" &>> /var/log/kops.log
+echo "$(docker push $IMAGE_URI)"  &>> /var/log/kops.log
+sleep 30
+echo "$(docker image ls $IMAGE_URI)" &>> /var/log/kops.log
+echo "$(kubectl set image deployment/website1 website1=$IMAGE_URI)" &>> /var/log/kops.log
