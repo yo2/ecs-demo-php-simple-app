d36e5e13f81077426baa78f65a8bc06f2a5ae52a
David Williams <david.williams@polarseven.com>
2018-03-25 20:02:59 +1100

    redeploy

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,3 +1,4 @@
 #1/bin/bash
-$IMAGE_URI=cat images.json | jq .[0]."imageUri" | sed 's/"//g'
+$IMAGE_URI=cat ../images.json | jq .[0]."imageUri" | sed 's/"//g'
+echo "Codedeploy IMAGE_URI = $IMAGE_URI"  &>> /var/log/kops.log
 echo "$(sudo kubectl set image deployment/website1 website1=$IMAGE_URI)" &>> /var/log/kops.log
